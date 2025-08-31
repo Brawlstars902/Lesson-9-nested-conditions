436421127aa2aabce189f0df95e1e5368f5ea0bf
Brawlstars902 <aparna.rautela@gmail.com>
2025-08-31 06:23:15 +0530

    Add files via upload

---
+++ Checking age.py
@@ -0,0 +1,10 @@
+print('Learning to code is only for bright minds from 10 to 20 years old.')
+age=int(input('Please enter your age in numbers:  '))
+if age>=10 and age<=20:
+ print('Your age is suitable for learning code.')
+elif age<10:
+ print('You are a baby and you do not have the brainpower to learn coding.')
+elif age>20:
+ print('You are to old to master the code.')
+else:
+ print('Invalide Input')
